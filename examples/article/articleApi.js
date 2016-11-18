27dc03f7e37e55d23607adc0593f46620ac1183e
sp00m <christophe.maillard@rocketmail.com>
2016-11-18 10:17:19 +0000

    [1.0.0] Updating document implemented

--- examples/article/articleApi.js
+++ examples/article/articleApi.js
@@ -6,7 +6,7 @@ module.exports = router => {
 
   api(router)
 
-    .get(articlePaths.article, function *getArticle() {
+    .get(articlePaths.article, function *findArticle() {
       const article = yield articleService.findOneById(this.params.id)
       if (!article) {
         return api.notFound()
@@ -16,14 +16,14 @@ module.exports = router => {
       }
     })
 
-    .get(articlePaths.articles, function *listArticles() {
+    .get(articlePaths.articles, function *findArticles() {
       const articles = yield articleService.find()
       return api.ok(articles)
         .forEach((resource, article) => resource.link("self", articlePaths.article(article._id)))
         .link("self", articlePaths.articles())
     })
 
-    .post(articlePaths.articles, function *createArticle() {
+    .post(articlePaths.articles, function *insertArticle() {
       const article = yield articleService.insertOne(this.request.body)
       return api.created(article)
         .link("self", articlePaths.article(article._id))
@@ -31,6 +31,16 @@ module.exports = router => {
 
     .put(articlePaths.article, function *replaceArticle() {
       const article = yield articleService.replaceOneById(this.params.id, this.request.body)
+      if (!article) {
+        return api.notFound()
+      } else {
+        return api.ok(article)
+          .link("self", articlePaths.article(this.params.id))
+      }
+    })
+
+    .patch(articlePaths.article, function *updateArticle() {
+      const article = yield articleService.updateOneById(this.params.id, this.request.body)
       if (!article) {
         return api.notFound()
       } else {
